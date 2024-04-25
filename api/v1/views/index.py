e0aafbee879e4c9bded0a999e93eb3739cc184fe
seyyar95 <heydrovsyyar@gmail.com>
2024-04-25 13:01:14 +0400

    Module documented

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This module contains the routes for the index of the API.
+"""
+
 from api.v1.views import app_views
 from flask import jsonify
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def return_status():
-    """Returns a JSON object"""
+    """Returns a JSON object with the status of the API.
+
+    Returns:
+        A JSON object containing the status of the API.
+
+    """
     return jsonify({"status": "OK"})
