cee88e965fab581996a933413fb78aec539ff7df
seyyar95 <heydrovsyyar@gmail.com>
2024-04-26 18:05:25 +0400

    HTTP acces control (CORS)

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,9 +8,11 @@ from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.errorhandler(404)
